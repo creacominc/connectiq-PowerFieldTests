9383c9f505c0cde9bd2efb296c71f0534532d5dc
creacominc <haroldt@creacom.ca>
2017-10-13 18:47:40 -0400

    removed mb_runner sub-module and copied useful parts of script to travis.sh.

--- travis.sh
+++ travis.sh
@@ -19,11 +19,88 @@ openssl pkcs8 -topk8 -inform PEM -outform DER -in "${PEM_FILE}" -out "${DER_FILE
 export MB_HOME="${SDK_DIR}"
 export MB_PRIVATE_KEY="${DER_FILE}"
 
-set -x 
-pwd
-ls -altr 
-ls -atlr source
-ls -altr source/connectiq-PowerField
-
 # mb_runner needs to know where the resource folder is.
 #./mb_runner/mb_runner.sh test `pwd` source/connectiq-PowerField/resources
+
+#
+# from mb_runner
+#
+
+PROJECT_HOME="${PWD}"
+RESOURCES_FOLDER="source/connectiq-PowerField/resources"
+SOURCE_FOLDER="source"
+
+#CONFIG_FILE="${PROJECT_HOME}/mb_runner.cfg"
+APP_NAME="PowerFieldTests"
+TARGET_DEVICE="edge_1000"
+TARGET_DEVICE="edge_1030"
+TARGET_DEVICE="edge820"
+TARGET_SDK_VERSION="2.3.0"
+
+MANIFEST_FILE="${PROJECT_HOME}/manifest.xml"
+
+
+RESOURCES="`cd /; find \"${PROJECT_HOME}/${RESOURCES_FOLDER}\"* -iname '*.xml' | tr '\n' ':'`"
+SOURCES="`cd /; find \"${PROJECT_HOME}/${SOURCE_FOLDER}\" -iname '*.mc' | tr '\n' ' '`"
+
+API_DB="${MB_HOME}/bin/api.db"
+PROJECT_INFO="${MB_HOME}/bin/projectInfo.xml"
+API_DEBUG="${MB_HOME}/bin/api.debug.xml"
+DEVICES="${MB_HOME}/bin/devices.xml"
+
+# **********
+# processing
+# **********
+
+# prepare sdk executables and apply "wine-ification", if not already done so ...
+
+if [ ! -e "${MB_HOME}/bin/monkeydo.bak" ] ; then
+    cp -a "${MB_HOME}/bin/monkeydo" "${MB_HOME}/bin/monkeydo.bak"
+    dos2unix "${MB_HOME}/bin/monkeydo"
+    chmod +x "${MB_HOME}/bin/monkeydo"
+    sed -i -e 's/"\$MB_HOME"\/shell/wine "\$MB_HOME"\/shell.exe/g' "${MB_HOME}/bin/monkeydo"
+fi
+
+if [ ! -e "${MB_HOME}/bin/monkeyc.bak" ] ; then
+    cp -a "${MB_HOME}/bin/monkeyc" "${MB_HOME}/bin/monkeyc.bak"
+    chmod +x "${MB_HOME}/bin/monkeyc"
+    dos2unix "${MB_HOME}/bin/monkeyc"
+fi
+
+function concat_params_for_build
+{
+    PARAMS+="--apidb \"${API_DB}\" "
+    PARAMS+="--device \"${TARGET_DEVICE}\" "
+    PARAMS+="--import-dbg \"${API_DEBUG}\" "
+    PARAMS+="--manifest \"${MANIFEST_FILE}\" "
+    PARAMS+="--output \"${APP_NAME}.prg\" "
+    PARAMS+="--project-info \"${PROJECT_INFO}\" "
+    PARAMS+="--sdk-version \"${TARGET_SDK_VERSION}\" "
+    PARAMS+="--unit-test "
+    PARAMS+="--devices \"${DEVICES}\" "
+    PARAMS+="--warn "
+    PARAMS+="--private-key \"${MB_PRIVATE_KEY}\" "
+    PARAMS+="--rez \"${RESOURCES}\" "
+}
+
+function run_mb_jar
+{
+	echo "java -jar \"${MB_HOME}/bin/monkeybrains.jar\" ${PARAMS} ${SOURCES}"
+    java -jar "${MB_HOME}/bin/monkeybrains.jar" ${PARAMS} ${SOURCES}
+}
+
+function run_tests
+{
+	echo "\"${MB_HOME}/bin/monkeydo\" \"${PROJECT_HOME}/${APP_NAME}.prg\" -t"
+    "${MB_HOME}/bin/monkeydo" "${PROJECT_HOME}/${APP_NAME}.prg" -t
+}
+
+
+cd ${PROJECT_HOME}
+concat_params_for_build
+run_mb_jar
+run_tests
+
+
+
+
